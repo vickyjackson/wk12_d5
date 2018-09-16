64f376ef1499a463c424819012e46d497fba8c82
Vicky Jackson <vicky@digitalstuff.net>
2018-09-16 15:39:44 +0100

    Added popup with launch details and embedded video - needs refactored!

--- src/app.js
+++ src/app.js
@@ -7,7 +7,12 @@ const LaunchpadView = require('./views/launchpad_view.js');
 document.addEventListener('DOMContentLoaded', () => {
 
   const dropdown = document.querySelector('#dropdown');
-
+  const close = document.querySelector('.close');
+  const overlay = document.querySelector('.overlay');
+  const popup = document.querySelector('.popup');
+  popup.style = 'display: none';
+  overlay.style = 'display: none';
+  close.addEventListener('click', handleClickClose);
 
   const launches = new Launches();
   launches.getData();
@@ -26,4 +31,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector('.grid-container');
   const launchpadView = new LaunchpadView(container);
   launchpadView.render();
+
 });
+
+const handleClickClose = function(){
+  const overlay = document.querySelector('.overlay');
+  const popup = document.querySelector('.popup');
+  popup.style = 'display: none';
+  overlay.style = 'display: none';
+}
--- src/views/launchpad_view.js
+++ src/views/launchpad_view.js
@@ -1,14 +1,14 @@
 const PubSub = require('../helpers/pub_sub.js');
 const Launches = require('../models/launches.js');
 
-const LaunchpadView = function(container){
+const LaunchpadView = function (container) {
   // .grid-container
   this.marker = null;
   this.container = container;
   this.detailsContainer = document.querySelector('.content-right-inner');
 };
 
-LaunchpadView.prototype.render = function(){
+LaunchpadView.prototype.render = function () {
 
   PubSub.subscribe('LaunchPads:selected-launchpad-ready', (event) => {
 
@@ -17,7 +17,7 @@ LaunchpadView.prototype.render = function(){
 
     this.detailsContainer.innerHTML = '';
 
-    if(!this.launchpad){
+    if (!this.launchpad) {
       map.flyTo({
         zoom: 0
       });
@@ -49,18 +49,18 @@ LaunchpadView.prototype.render = function(){
 };
 
 LaunchpadView.prototype.getLaunchesById = function (container) {
-  
+
   // we only want to register this once and get it added to a container
 
   console.log("launchpadview - subscribing to launches by id ready");
   PubSub.subscribe('Launches:launches-by-id-ready', (event) => {
-    
+
     console.log('recieived launched-by-id');
-    
+
     this.launchesById = event.detail;
 
     const rocketsLaunchedHeading = document.createElement('h4');
-    if (this.launchesById.length === 0){
+    if (this.launchesById.length === 0) {
       rocketsLaunchedHeading.textContent = `There is no mission data for this launch site.`;
       this.detailsContainer.appendChild(rocketsLaunchedHeading);
     } else {
@@ -70,16 +70,77 @@ LaunchpadView.prototype.getLaunchesById = function (container) {
 
     this.launchesById.forEach((launch) => {
       const launchButton = document.createElement('button');
+      const overlay = document.querySelector('.overlay');
+      const popup = document.querySelector('.popup');
+      const popupContent = document.querySelector('.popup-content');
       launchButton.innerHTML = `<i class="fas fa-rocket"></i><br> ${launch.mission_name}`;
       container.appendChild(launchButton);
+      launchButton.addEventListener('click', () => {
+        popupContent.innerHTML = '';
+        popup.style = 'display: flex';
+        overlay.style = 'display: block';
+
+        const introContainer = document.createElement('div');
+        introContainer.classList.add('intro-container');
+        popupContent.appendChild(introContainer);
+
+        const launchHeading = document.createElement('h3');
+        launchHeading.textContent = launch.mission_name;
+        introContainer.appendChild(launchHeading);
+
+        const launchFlightNo = document.createElement('p');
+        launchFlightNo.textContent = `Flight number: ${launch.flight_number}`;
+        introContainer.appendChild(launchFlightNo);
+
+        const launchDate = document.createElement('p');
+        launchDate.textContent = `Launch date: ${launch.launch_date_utc}`;
+        introContainer.appendChild(launchDate);
+
+        const launchSubHeading = document.createElement('p');
+        launchSubHeading.textContent = `Rocket: ${launch.rocket.rocket_name}`;
+        introContainer.appendChild(launchSubHeading);
+
+        const launchSuccess = document.createElement('p');
+        launchSuccess.textContent = `Launch success: ${launch.launch_success}`;
+        introContainer.appendChild(launchSuccess);
+
+        const detailsContainer = document.createElement('div');
+        detailsContainer.classList.add('details-container');
+        const launchDetails = document.createElement('p');
+        launchDetails.textContent = launch.details;
+        detailsContainer.appendChild(launchDetails);
+        popupContent.appendChild(detailsContainer);
+
+
+        function getId (url) {
+          var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+          var match = url.match(regExp);
+      
+          if (match && match[2].length == 11) {
+              return match[2];
+          } else {
+              return 'error';
+          }
+        }
+      
+        var videoId = getId(launch.links.video_link);
+        console.log(videoId);
+        console.log(launch.links.video_link);
+      
+        var iframeMarkup = '<iframe width="100%" src="https://www.youtube.com/embed/' 
+          + videoId + '" frameborder="0" allowfullscreen></iframe>';
+
+        const launchVideo = document.createElement('div');
+        launchVideo.classList.add('launch-video');
+        launchVideo.innerHTML = iframeMarkup;
+        popupContent.appendChild(launchVideo);
+      })
     });
-
-
   });
-  
+
 };
 
-LaunchpadView.prototype.drawMapMarker = function(){
+LaunchpadView.prototype.drawMapMarker = function () {
   var el = document.createElement('div');
   el.className = 'marker';
 
@@ -90,8 +151,8 @@ LaunchpadView.prototype.drawMapMarker = function(){
   launchpadCoordinates.push(this.launchpad.location.latitude);
 
   this.marker = new mapboxgl.Marker(el)
-  .setLngLat(launchpadCoordinates)
-  .addTo(map);
+    .setLngLat(launchpadCoordinates)
+    .addTo(map);
   console.log('adding map marker');
 
   map.flyTo({
@@ -103,8 +164,8 @@ LaunchpadView.prototype.drawMapMarker = function(){
 
 };
 
-LaunchpadView.prototype.removeMapMarker = function(){
-  if (this.marker !== null){
+LaunchpadView.prototype.removeMapMarker = function () {
+  if (this.marker !== null) {
     this.marker.remove();
     console.log('removing map marker');
   }
